e2a10715ae9b9218f6881073fec81391b93934ed
Artem Arkhipov <zaq12123@mail.ru>
2021-06-28 21:04:59 +0300

    add models in admin menu

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from main.models import *
 
 # Register your models here.
+
+myModels = [Company, NameOfSpecification, TypeOfSpecification,
+            UnitOfMeasurementOfSpecification, SpecificationsOfProduct,
+            Product, Warehouse, Offer, Document, Deal]
+
+admin.site.register(myModels)
