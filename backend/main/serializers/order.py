1ed597d5fc5ff76d03a9703e8cea0d14cd78789d
sergeybabayan <sergey.babayan.v@gmail.com>
2021-07-11 12:02:54 +0300

    added order accept

--- backend/main/serializers/order.py
+++ backend/main/serializers/order.py
@@ -2,6 +2,7 @@ from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 from .. import models
+from ..enums import TaxTypes
 
 
 class OrderSerializer(serializers.ModelSerializer):
@@ -10,8 +11,10 @@ class OrderSerializer(serializers.ModelSerializer):
     cost = serializers.IntegerField(source="offer.cost")
     period_of_export = serializers.IntegerField(source="offer.period_of_export")
     cost_by_tonne = serializers.SerializerMethodField("get_cost_by_tonne")
-    tax_type = serializers.CharField(source="offer.tax_type")
-    provider = serializers.CharField(source="offer.company_name")
+    tax_type = serializers.ChoiceField(
+        source="offer.tax_type", choices=list(TaxTypes.readable())
+    )
+    provider_name = serializers.CharField(source="offer.company_name")
 
     @extend_schema_field(OpenApiTypes.INT)
     def get_cost_by_tonne(self, instance: models.Order) -> int:
