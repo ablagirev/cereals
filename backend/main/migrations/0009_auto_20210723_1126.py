00c28b5e3e7e176113eec8e0bd11ad707f51353e
sergeybabayan <sergey.babayan.v@gmail.com>
2021-07-23 15:11:51 +0300

    added offer spec updating

--- backend/main/migrations/0009_auto_20210723_1126.py
+++ backend/main/migrations/0009_auto_20210723_1126.py
@@ -4,29 +4,77 @@ from django.db import migrations, models
 import django.db.models.deletion
 
 
+def reverse(apps, schema_editor):
+    Category = apps.get_model("products", "Category")
+    db_alias = schema_editor.connection.alias
+    c = Category.objects.using(db_alias).filter(name="test").first()
+    if c:
+        c.delete()
+
+
+def up(apps, schema_editor):
+    Category = apps.get_model("products", "Category")
+    db_alias = schema_editor.connection.alias
+    Category.objects.using(db_alias).bulk_create(
+        [Category(name="test"),]
+    )
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main', '0008_merge_20210721_1643'),
+        ("main", "0008_merge_20210721_1643"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Category',
+            name="Category",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=255)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=255)),
             ],
         ),
         migrations.AlterField(
-            model_name='document',
-            name='type_doc',
-            field=models.CharField(choices=[('other', 'Прочее'), ('specification', 'Спецификация'), ('contract_for_signing', 'Договор на подписание'), ('payment_invoice', 'Счет на оплату'), ('quality_certificate', 'Сертификат качества'), ('contract', 'Договор'), ('payment_order', 'Платежное поручение'), ('verification_act', 'Акт сверки'), ('book_of_purchases_or_sales', 'Книга покупок/продаж'), ('letter_for_refund', 'Письмо на возврат'), ('loading_plan', 'План погрузки'), ('report_on_the_shipped_goods', 'Отчет о погруженном товаре'), ('additional_payment_invoice', 'Счет на доплату')], default='other', max_length=250, verbose_name='Тип документа'),
+            model_name="document",
+            name="type_doc",
+            field=models.CharField(
+                choices=[
+                    ("other", "Прочее"),
+                    ("specification", "Спецификация"),
+                    ("contract_for_signing", "Договор на подписание"),
+                    ("payment_invoice", "Счет на оплату"),
+                    ("quality_certificate", "Сертификат качества"),
+                    ("contract", "Договор"),
+                    ("payment_order", "Платежное поручение"),
+                    ("verification_act", "Акт сверки"),
+                    ("book_of_purchases_or_sales", "Книга покупок/продаж"),
+                    ("letter_for_refund", "Письмо на возврат"),
+                    ("loading_plan", "План погрузки"),
+                    ("report_on_the_shipped_goods", "Отчет о погруженном товаре"),
+                    ("additional_payment_invoice", "Счет на доплату"),
+                ],
+                default="other",
+                max_length=250,
+                verbose_name="Тип документа",
+            ),
         ),
+        migrations.RunPython(up, reverse),
         migrations.AddField(
-            model_name='culture',
-            name='category',
-            field=models.ForeignKey(default=0, on_delete=django.db.models.deletion.CASCADE, to='main.category'),
+            model_name="culture",
+            name="category",
+            field=models.ForeignKey(
+                default=1,
+                on_delete=django.db.models.deletion.CASCADE,
+                to="main.category",
+            ),
             preserve_default=False,
         ),
     ]
