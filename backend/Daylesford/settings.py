8c78c4969dae138007fde7c08119a548cc9e6c4f
sergeybabayan <sergey.babayan.v@gmail.com>
2021-07-27 12:22:19 +0300

    media fix

--- backend/Daylesford/settings.py
+++ backend/Daylesford/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.environ.get("MEDIA_ROOT", os.path.join(BASE_DIR, "media"))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
